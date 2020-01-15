b83c222d4803e8dfe08d986dd8d1919dc7b19f89
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-15 21:07:32 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_15_205952) do
-
-  create_table "dogs", force: :cascade do |t|
-    t.string "name"
-    t.string "breed"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
